f00cddb2ac64210f80efe8909a34582dffdb5149
Tintin Boulsard <76255578+Tintin1068@users.noreply.github.com>
2023-08-03 13:30:18 +0200

    Update MotorActuatorChecker-fr.py

--- MotorActuatorChecker-fr.py
+++ MotorActuatorChecker-fr.py
@@ -1,36 +1,48 @@
-# Définition des constantes
-g = 9.81 # Accélération de la pesanteur en m/s²
+g = 9.81
 L = float(input("Course du vérin (en mm) : ")) / 1000
-# Course du vérin en m (0.205 pour la version SRT150)
-p = 5 # Pas de vis en mm
-
-# Saisie des données par l'utilisateur
+p = 5 
 m = float(input("Poids du châssis (en kg) : "))
-s = float(input("Vitesse de déplacement désirée (en mm/s) : "))
+s = float(input("Vitesse de déplacement maximale désirée (en mm/s) : "))
 t = 1
 mu = 0.01
-d = float(input("Distance entre l'axe du moteur et le point d'application de la force (en mm) : "))
+L = float(input("Course du vérin (en mm) : ")) / 1000
 n = int(input("Nombre de vérins : "))
-k = float(input("Rendement du vérin : "))
+k = 0.8
 
+if n == 1:
+    d = 100
+else:
+    if n == 2:
+        d = 55
+    else:
+        if n == 3:
+            d = 35
+        else:
+            if n == 4:
+                d = 25
+            else:
+                if n == 5:
+                    d = 20
+                else:
+                    if n == 6:
+                        d = 16.6666
+                    else:
+                        if n == 7:
+                            d = 14.2857
+                        else:
+                            if n == 8:
+                                d = 12.5
+                            else:
+                                if n > 8:
+                                    print("Le nombre de vérins ne peut pas dépasser 8.")
+                                    d = 0
 
 a = 2 * s / 1000 / t**2
-
-# Calcul de la force exercée par le vérin
 F = k * m * a + mu * k * m * g
-
-
-# Calcul du couple du moteur
 C = F * d / 1000
-
-# Calcul de la vitesse de rotation du moteur
-v = s / t / p # Vitesse de déplacement du vérin en m/s
-RPM = v * 60 # Vitesse de rotation du moteur en tours par minute
-
-if 4 != n:
-    div = 4 / n
-    C = F * d / 1000 * div
-# Affichage du résultat
+v = s / t / p
+RPM = v * 60
+div = 4 / n
+C = F * d / 1000 * div
 print(f"Le couple minimal des moteurs sur les vérins SRT100 est de {C:.2f} N.m")
-print(f"La vitesse de rotation maximale des moteurs sur les vérins SRT100 est de {RPM:.2f} tours par minute")
-
+print(f"La vitesse de rotation maximale des moteurs est de {RPM:.2f} tours par minute.")
